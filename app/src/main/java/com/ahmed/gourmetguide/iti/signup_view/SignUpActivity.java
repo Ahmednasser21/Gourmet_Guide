1eabaa3c72d054512ee0aa7c89f4cda475f3ca5c
Ahmed Nasser <ahmed.nasser.11097@gmail.com>
2024-08-10 13:02:07 +0300

    Finishing sign up with all three methods, replace warnings from SignUpWithEmail with setError on edit texts and adding shared preference boolean to check if user is already logged in to direct him to Main Activity.

--- app/src/main/java/com/ahmed/gourmetguide/iti/signup_view/SignUpActivity.java
+++ app/src/main/java/com/ahmed/gourmetguide/iti/signup_view/SignUpActivity.java
@@ -1,22 +1,35 @@
 package com.ahmed.gourmetguide.iti.signup_view;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
+import com.ahmed.gourmetguide.iti.MainActivity;
 import com.ahmed.gourmetguide.iti.R;
 
 
 public class SignUpActivity extends AppCompatActivity {
 
     NavController navController;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_login_file_key),MODE_PRIVATE);
+        boolean loggedIn = sharedPreferences.getBoolean(getString(R.string.preferences_is_logged_in),false);
+
+        if (loggedIn){
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
         setContentView(R.layout.activity_sign_up);
+
         navController = Navigation.findNavController(this,R.id.fragmentContainerView);
+
     }
 }
